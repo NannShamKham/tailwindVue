95735adeb3769ebcf47497ce6ad8e3c99ea1807e
thanhitkeaung09 <thanhtikeaungupwork@gmail.com>
2022-08-29 11:50:33 +0630

    tailwind css fixed

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 
 // import './assets/main.css'
-import './assets/css/input.css'
+import "./assets/css/input.css"
 
 const app = createApp(App)
 
